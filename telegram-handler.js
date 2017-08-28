1c9c7c446b9fcb830a2c79be98dd19e671ccdd38
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-28 21:57:20 +0700

    bug fix

--- telegram-handler.js
+++ telegram-handler.js
@@ -2,11 +2,14 @@ const axios = require('axios')
 const TELEGRAM_API = 'https://api.telegram.org/bot' + process.env.TELEGRAM_BOT_TOKEN + '/'
 
 module.exports = (req, res) => {
+  let message = req.body.message
+  if (message && message.text)
+    replyText(message.chat.id, message.message_id, message.text + ' juga')
   res.sendStatus(200)
-  let update = req.body
-  if (update.text) replyText(update.chat.id, update.message.message_id, update.text + ' juga')
 }
 
 function replyText(chatId, messageId, text) {
   return axios.post(TELEGRAM_API+'sendMessage', {chat_id: chatId, reply_to_message_id: messageId, text})
+  .then(console.log)
+  .catch(console.log)
 }
