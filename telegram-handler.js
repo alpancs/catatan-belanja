39e31e1f70cb6160d65528e6ec66fca635cbb47c
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-28 21:48:34 +0700

    reply juga

--- telegram-handler.js
+++ telegram-handler.js
@@ -1,4 +1,12 @@
+const axios = require('axios')
+const TELEGRAM_API = 'https://api.telegram.org/bot' + process.env.TELEGRAM_BOT_TOKEN + '/'
+
 module.exports = (req, res) => {
-  console.log(JSON.stringify(req.body))
   res.sendStatus(200)
+  let update = req.body
+  if (update.text) replyText(update.chat.id, update.message.message_id, update.text + ' juga')
+}
+
+function replyText(chatId, messageId, text) {
+  return axios.post(TELEGRAM_API+'sendMessage', {chat_id: chatId, reply_to_message_id: messageId, text})
 }
