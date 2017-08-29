affdfec095dd7f873c12d14f31d26f1b8acde64c
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-29 14:50:57 +0700

    builtin promise

--- shopping-item.js
+++ shopping-item.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+
+mongoose.Promise = Promise
 mongoose.connect(process.env.MONGODB_URL, {useMongoClient: true})
 
 module.exports = mongoose.model('ShoppingItem', {owner: Number, name: String, price: Number})
