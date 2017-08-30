4843334c1d3581e60d35040f9631bd2923af4d0a
Alfan Nur Fauzan <alpancs@gmail.com>
2017-08-30 13:27:09 +0700

    make pretty

--- telegram-handler.js
+++ telegram-handler.js
@@ -41,7 +41,7 @@ let showSummary = (message) => {
     let dailySum = monthlyShoppingItems.filter(daily).reduce(sum, 0)
     let weeklySum = monthlyShoppingItems.filter(weekly).reduce(sum, 0)
     let monthlySum = monthlyShoppingItems.reduce(sum, 0)
-    let text = `Belanja hari ini: ${dailySum}\nBelanja pekan ini: ${weeklySum}\nBelanja bulan ini: ${monthlySum}`
+    let text = `Belanja hari ini: ${pretty(dailySum)}\nBelanja pekan ini: ${pretty(weeklySum)}\nBelanja bulan ini: ${pretty(monthlySum)}`
     replyText(message.chat.id, message.message_id, text)
   }, console.log)
 }
@@ -59,3 +59,13 @@ let sum = (acc, shoppingItem) => acc + shoppingItem.price
 let daily = (shoppingItem) => gteFilter(shoppingItem, beginningOfDay(new Date()))
 let weekly = (shoppingItem) => gteFilter(shoppingItem, beginningOfWeek(new Date()))
 let gteFilter = (shoppingItem, minDate) => (shoppingItem) => shoppingItem.createdAt >= minDate
+
+let pretty = (number) => {
+  let text = String(number)
+  let result = ''
+  while (text.length > 3) {
+    result = '.' + text.slice(-3) + result
+    text = text.slice(0, -3)
+  }
+  return text.slice(-3) + result
+}
