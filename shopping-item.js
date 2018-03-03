61403be602c5921b12634ed25f2ad41ac4207794
Alfan Nur Fauzan <alpancs@gmail.com>
2018-03-03 08:05:45 +0700

    bug fix

--- shopping-item.js
+++ shopping-item.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose')
 mongoose.Promise = Promise
 mongoose.connect(process.env.MONGODB_URL, {useMongoClient: true})
 
-let shoppingItemSchema = {
+let shoppingItemSchema = new mongoose.Schema({
   owner: Number,
   name: String,
   price: Number,
   createdAt: {type: Date, default: Date.now},
-}
+})
 
 shoppingItemSchema.methods.simpleDate = () => `${this.createdAt.getDate()}/${this.createdAt.getMonth()+1}`
 
